c30f03e4ebded33fc270a14aaab9d743e4fc2c88
Michael Kuehn <mkuehn10@gmail.com>
2016-07-18 17:10:35 -0400

    create search functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    var type;
+    var level;
+
+    function createColorImgSrc(src) {
+        return 'images/' + src.slice(7, src.length - 4) + ' - color.png';
+    }
+
+    function removeColorImgSrc(src) {
+        if (src.includes('color')) {
+            return 'images/' + src.slice(7, 10) + '.png';
+        } else {
+            return src;
+        }
+    }
+
+    function getSelectValue(name) {
+        return $('select[name=' + name + ']').val();
+
+    }
+
+    function getImagesInMatrix() {
+        return $('.picture-matrix div img');
+    }
+
+    function tagContainsClass(cls, tags) {
+        return ($.inArray(cls, tags) != -1);
+    }
+
+    $('#search-form').submit(function(e) {
+        e.preventDefault();
+        type = getSelectValue('type');
+        level = getSelectValue('level');
+        var query = getImagesInMatrix();
+        $.each(query, function(n, item) {
+            $(item).attr('src', removeColorImgSrc($(item).attr('src')));
+            var currentTagClasses = $(item).attr('class').split(' ');
+            var containsType = tagContainsClass(type, currentTagClasses);
+            var containsLevel = tagContainsClass(level, currentTagClasses);
+            if (containsType || containsLevel) {
+                $(item).attr('src', createColorImgSrc($(item).attr('src')));
+            }
+        })
+    })
+});
